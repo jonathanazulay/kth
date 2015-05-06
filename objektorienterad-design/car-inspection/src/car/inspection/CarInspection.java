357ddfa212b17151c20c3dfecc24c81615b19316
Jonathan Azulay <jonathanazulay@me.com>
2015-05-06 20:54:08 +0200

    make package private

--- objektorienterad-design/car-inspection/src/car/inspection/CarInspection.java
+++ objektorienterad-design/car-inspection/src/car/inspection/CarInspection.java
@@ -3,7 +3,7 @@ package car.inspection;
 import se.kth.iv1350.carinspection.controller.Controller;
 import se.kth.iv1350.carinspection.view.View;
 
-public class CarInspection {
+class CarInspection {
     public static void main(String[] args) {
         Controller controller = new Controller();
         View view = new View(controller);
