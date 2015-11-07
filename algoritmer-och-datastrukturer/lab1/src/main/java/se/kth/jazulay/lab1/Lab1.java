11bcfd0161184f2c1e18a2067e27a068847c9f02
Jonathan Azulay <jonathanazulay@me.com>
2015-11-07 22:33:31 +0100

    blah

--- algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/Lab1.java
+++ algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/Lab1.java
@@ -13,5 +13,10 @@ public class Lab1 {
     public static void main(String[] args) {
         RecursivePascal pascalPrinter = new RecursivePascal(true);
         pascalPrinter.printPascal(10);
+        
+        System.out.println("");
+        
+        RecursivePascal revPascalPrinter = new RecursivePascal(false);
+        revPascalPrinter.printPascal(10);
     }
 }
