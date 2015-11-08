0013b26415f1ca29ff6901449903eec47866a2c0
Jonathan Azulay <jonathanazulay@me.com>
2015-11-08 20:04:24 +0100

    class stub

---
+++ algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/IterativePascal.java
@@ -0,0 +1,15 @@
+package se.kth.jazulay.lab1;
+
+public class IterativePascal implements Pascal {
+
+    @Override
+    public void printPascal(int n) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int binom(int n, int k) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
