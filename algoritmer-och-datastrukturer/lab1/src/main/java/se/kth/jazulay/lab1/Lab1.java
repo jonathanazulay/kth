2b9f55e26333ed779393e7d1b57273a15eadcc1b
Jonathan Azulay <jonathanazulay@me.com>
2015-11-08 20:04:00 +0100

    measure execution time

--- algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/Lab1.java
+++ algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/Lab1.java
@@ -5,18 +5,28 @@
  */
 package se.kth.jazulay.lab1;
 
+import com.google.common.base.Stopwatch;
+
 /**
  *
  * @author jonathanazulay
  */
 public class Lab1 {
     public static void main(String[] args) {
-        RecursivePascal pascalPrinter = new RecursivePascal(true);
-        pascalPrinter.printPascal(10);
+        Stopwatch swCached = Stopwatch.createStarted();
+        Pascal pascalPrinter = new RecursivePascal(false);
+        pascalPrinter.printPascal(30);
+        swCached.stop();
+        
+        Stopwatch swUncached = Stopwatch.createStarted();
+        Pascal unchached = new UncachedRecursivePascal(false);
+        unchached.printPascal(30);
+        swUncached.stop();
         
         System.out.println("");
+        System.out.println("");
         
-        RecursivePascal revPascalPrinter = new RecursivePascal(false);
-        revPascalPrinter.printPascal(10);
+        System.out.println(swCached);
+        System.out.println(swUncached);
     }
 }
