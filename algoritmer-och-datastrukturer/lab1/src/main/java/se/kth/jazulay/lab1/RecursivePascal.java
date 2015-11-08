ccb72367d7c07419755d08481f15909e27748446
Jonathan Azulay <jonathanazulay@me.com>
2015-11-08 20:02:56 +0100

    cache calculations result, faster execution

--- algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/RecursivePascal.java
+++ algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/RecursivePascal.java
@@ -1,10 +1,14 @@
 package se.kth.jazulay.lab1;
 
-public class RecursivePascal {
+import java.util.ArrayList;
+
+public class RecursivePascal implements Pascal {
     boolean reverse;
-    
+    ArrayList<ArrayList<Integer>> rows;
+
     public RecursivePascal (boolean reverse) {
         this.reverse = reverse;
+        this.rows = new ArrayList();
     }
     
     public void printPascal (int n) {
@@ -21,6 +25,34 @@ public class RecursivePascal {
         if (n <= 0 || k >= n || k == 0) {
             return 1;
         }
-        return binom(n - 1, k - 1) + binom(n - 1, k);
+        
+        Integer result = this.fetchCached(n, k);
+        if (result == null) {
+            result = binom(n - 1, k - 1) + binom(n - 1, k);
+            this.storeCached(n, k, result);
+        }
+        return result;
+    }
+    
+    private Integer fetchCached (int n, int k) {
+        try {
+            return this.rows.get(n).get(k);
+        } catch (NullPointerException e) {
+            return null;
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+    
+    private void storeCached (int n, int k, int value) {
+        while (this.rows.size() <= n) {
+            this.rows.add(new ArrayList<Integer>());
+        }
+        
+        while (this.rows.get(n).size() <= k) {
+            this.rows.get(n).add(null);
+        }
+        
+        this.rows.get(n).set(k, value);  
     }
 }
