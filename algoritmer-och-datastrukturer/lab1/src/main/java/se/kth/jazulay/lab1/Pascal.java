af4847793125c71b41b9df9e311d80024d76a411
Jonathan Azulay <jonathanazulay@me.com>
2015-11-08 20:02:29 +0100

    add interface

---
+++ algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/Pascal.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.jazulay.lab1;
+
+/**
+ *
+ * @author jonathanazulay
+ */
+public interface Pascal {
+    public void printPascal (int n);
+    public int binom (int n, int k);
+}
