97259c5afc97d28658e9580a7e3935af9b19594a
Jonathan Azulay <jonathanazulay@me.com>
2015-11-10 03:16:51 +0100

    cleanup

--- algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/Lab1.java
+++ algoritmer-och-datastrukturer/lab1/src/main/java/se/kth/jazulay/lab1/Lab1.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package se.kth.jazulay.lab1;
 
 import com.google.common.base.Stopwatch;
 
-/**
- *
- * @author jonathanazulay
- */
 public class Lab1 {
     public static void main(String[] args) {
-        int levels = 23;
+        int levels = 13;
         
         Stopwatch[] stopwatches = {
             Stopwatch.createUnstarted(),
