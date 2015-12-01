6d86feb7a6e21989d4d21c976286b0db43756857
Jonathan Azulay <jonathanazulay@me.com>
2015-12-01 02:15:38 +0100

    undocumented things

--- algoritmer-och-datastrukturer/project1/src/main/java/se/kth/jazulay/project1/TinySearchEngine.java
+++ algoritmer-och-datastrukturer/project1/src/main/java/se/kth/jazulay/project1/TinySearchEngine.java
@@ -2,7 +2,6 @@ package se.kth.jazulay.project1;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Random;
 
 import se.kth.id1020.TinySearchEngineBase;
 import se.kth.id1020.util.Attributes;
@@ -11,55 +10,67 @@ import se.kth.id1020.util.Word;
 
 public class TinySearchEngine implements TinySearchEngineBase {
 
-    private final ArrayList<Word> words = new ArrayList();
-    private final ArrayList<Attributes> attributes = new ArrayList();
+    /**
+     * This ArrayList Contains one WordAttributesList for each distinct(!) word.
+     * 
+     * This makes indexing really fast when the same word
+     * occurs many times because the same word does not have
+     * to be sorted multiple times.
+     */
+    private final ArrayList<WordAttributesList> words = new ArrayList();
+    
+    class WordAttributesList {
+        public String word;
+        private final ArrayList<Word> words = new ArrayList();
+        private final ArrayList<Attributes> attributes = new ArrayList();
+
+        public WordAttributesList (String word) {
+            this.word = word;
+        }
+        
+        public void add (Word word, Attributes attrs) {
+            if (this.word.equals(word.word)) {
+                this.words.add(word);
+                this.attributes.add(attrs);
+            } else {
+                throw new Error("word has to match the word for this collection (" + this.word + ")");
+            }
+        }
+    }
     
     @Override
     public void insert(Word word, Attributes atrbts) {
         int insertAt = this.sortedPosition(word);
-        this.words.add(insertAt, word);
-        this.attributes.add(insertAt, atrbts);
+        boolean wordPreviouslyAdded = false;
+        
+        if (insertAt < this.words.size()) {
+            wordPreviouslyAdded = this.words.get(insertAt).word.equals(word.word);
+        }
+        
+        if (wordPreviouslyAdded) {
+            this.words.get(insertAt).add(word, atrbts);
+        } else {
+            WordAttributesList newList = new WordAttributesList(word.word);
+            newList.add(word, atrbts);
+            this.words.add(insertAt, newList);
+        }
     }
 
     @Override
     public List<Document> search(String string) {
-        int low = 0;
-        int high = this.words.size() - 1;
-
-        int middle = 0;
-        while (high >= low) {
-            middle = (low + high) / 2;            
-            int compareResult = this.words.get(middle).word.compareTo(string);
-            
-            if (compareResult < 0) {
-                low = middle + 1;
-            } else if (compareResult > 0) {
-                high = middle - 1;
-            } else {
-                return null;
-            }
-        }
-        
         return null;
     }
     
     private int sortedPosition (Word word) {
-        int low = 0;
-        int high = this.words.size() - 1;
-
-        int middle = 0;
-        while (high >= low) {
-            middle = (low + high) / 2;            
-            int compareResult = this.words.get(middle).word.compareTo(word.word);
-            
-            if (compareResult < 0) {
-                low = middle + 1;
-            } else if (compareResult > 0) {
-                high = middle - 1;
-            } else {
-                break;
-            }
+        int lo = 0;
+        int hi = this.words.size() - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int comparison = word.word.compareTo(this.words.get(mid).word);
+            if      (comparison < 0) hi = mid - 1;
+            else if (comparison > 0) lo = mid + 1;
+            else return mid;
         }
-        return middle;        
+        return lo;
     }
 }
