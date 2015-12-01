bd5bda362f8096f43c36e210d1264a7a33308b3c
Jonathan Azulay <jonathanazulay@me.com>
2015-12-01 03:36:28 +0100

    things

--- algoritmer-och-datastrukturer/project1/src/main/java/se/kth/jazulay/project1/TinySearchEngine.java
+++ algoritmer-och-datastrukturer/project1/src/main/java/se/kth/jazulay/project1/TinySearchEngine.java
@@ -21,8 +21,8 @@ public class TinySearchEngine implements TinySearchEngineBase {
     
     class WordAttributesList {
         public String word;
-        private final ArrayList<Word> words = new ArrayList();
-        private final ArrayList<Attributes> attributes = new ArrayList();
+        public final ArrayList<Word> words = new ArrayList();
+        public final ArrayList<Attributes> attributes = new ArrayList();
 
         public WordAttributesList (String word) {
             this.word = word;
@@ -40,14 +40,14 @@ public class TinySearchEngine implements TinySearchEngineBase {
     
     @Override
     public void insert(Word word, Attributes atrbts) {
-        int insertAt = this.sortedPosition(word);
-        boolean wordPreviouslyAdded = false;
+        int insertAt = this.sortedPosition(word.word);
         
+        boolean wordExists = false;
         if (insertAt < this.words.size()) {
-            wordPreviouslyAdded = this.words.get(insertAt).word.equals(word.word);
+            wordExists = this.words.get(insertAt).word.equals(word.word);
         }
         
-        if (wordPreviouslyAdded) {
+        if (wordExists) {
             this.words.get(insertAt).add(word, atrbts);
         } else {
             WordAttributesList newList = new WordAttributesList(word.word);
@@ -58,15 +58,37 @@ public class TinySearchEngine implements TinySearchEngineBase {
 
     @Override
     public List<Document> search(String string) {
-        return null;
+        List<Document> searchResult = new ArrayList();
+        
+        int result = this.binarySearch(string);
+        
+        if (result != -1) {
+            for (Attributes attribute : this.words.get(result).attributes) {
+                
+            }
+        }
+        return searchResult;
     }
-    
-    private int sortedPosition (Word word) {
+
+    private int binarySearch (String word) {
         int lo = 0;
         int hi = this.words.size() - 1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            int comparison = word.word.compareTo(this.words.get(mid).word);
+            int comparison = word.compareTo(this.words.get(mid).word);
+            if      (comparison < 0) hi = mid - 1;
+            else if (comparison > 0) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+    
+    private int sortedPosition (String word) {
+        int lo = 0;
+        int hi = this.words.size() - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            int comparison = word.compareTo(this.words.get(mid).word);
             if      (comparison < 0) hi = mid - 1;
             else if (comparison > 0) lo = mid + 1;
             else return mid;
