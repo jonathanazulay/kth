87dbaffbe2c08ab33cc283c1be16291ab9e5b0d8
Jonathan Azulay <jonathanazulay@me.com>
2015-12-02 14:47:28 +0100

    readability

--- algoritmer-och-datastrukturer/project1/src/main/java/se/kth/jazulay/project1/QueryParser.java
+++ algoritmer-och-datastrukturer/project1/src/main/java/se/kth/jazulay/project1/QueryParser.java
@@ -3,6 +3,10 @@ package se.kth.jazulay.project1;
 import java.util.List;
 
 public class QueryParser {
+    String delimiter = "orderBy";
+    String[] properties = new String[]{"count", "popularity", "occurrence"};
+    String[] directions = new String[]{"asc", "desc"};
+    
     OrderableSearchEngine searchEngine;
     
     public QueryParser(OrderableSearchEngine engine) {
@@ -11,15 +15,40 @@ public class QueryParser {
 
     public <T> List<T> execute (String query) {
         String[] strings = query.split("\\s+");
-        
-        if (strings.length >= 4 && strings[strings.length - 3].equals("orderBy")) {
-            String[] searchTerms = new String[strings.length - 3];
-            for (int i = 0; i < strings.length - 3; i += 1) {
-                searchTerms[i] = strings[i];
+        boolean couldBeQuery = strings.length >= 4;
+        boolean isQuery = couldBeQuery && strings[strings.length - 3].equals(delimiter);
+   
+        if (isQuery) {
+            String property = strings[strings.length - 2];
+            String direction = strings[strings.length - 1];
+            
+            if (isDirection(direction) && isProperty(property)) {
+                return searchEngine.search(extractSearchTerms(strings), property, direction);
             }
-            return searchEngine.search(searchTerms, strings[strings.length - 2], strings[strings.length - 1]);
-        } else {
-            return searchEngine.search(strings, null, null);
         }
+        return searchEngine.search(strings, null, null);
+    }
+    
+    private boolean isProperty (String test) {
+        for (String property : this.properties) {
+            if (test.equals(property)) { return true; }
+        }
+        return false;
+    }
+    
+    private boolean isDirection (String test) {
+        for (String property : this.directions) {
+            if (test.equals(property)) { return true; }
+        }
+        return false;
+    }
+    
+    // deletes last 3 elements from string array
+    private String[] extractSearchTerms (String[] query) {
+        String[] searchTerms = new String[query.length - 3];
+        for (int i = 0; i < query.length - 3; i += 1) {
+            searchTerms[i] = query[i];
+        }
+        return searchTerms;
     }
 }
