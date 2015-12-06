0d5239a01efe375a3b221ed5f3343f6659366a98
Jonathan Azulay <jonathanazulay@me.com>
2015-12-06 20:11:29 +0100

    add some test sequence

--- algoritmer-och-datastrukturer/lab4/src/main/java/se/kth/jazulay/lab4/Main.java
+++ algoritmer-och-datastrukturer/lab4/src/main/java/se/kth/jazulay/lab4/Main.java
@@ -2,6 +2,11 @@ package se.kth.jazulay.lab4;
 
 public class Main {
     public static void main (String[] args) {
-        
+        Trie t = new Trie();
+        t.put("hej");
+        t.put("hej");
+        t.put("hejda");
+        t.put("hejdara");
+        t.put("ost");
     }
 }
