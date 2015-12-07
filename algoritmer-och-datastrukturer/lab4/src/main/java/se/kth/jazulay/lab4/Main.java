e9c1b06db3a973cee5cf6bb5657d39bd01adffc0
Jonathan Azulay <jonathanazulay@me.com>
2015-12-07 01:59:33 +0100

    fix formatting, add time measure

--- algoritmer-och-datastrukturer/lab4/src/main/java/se/kth/jazulay/lab4/Main.java
+++ algoritmer-och-datastrukturer/lab4/src/main/java/se/kth/jazulay/lab4/Main.java
@@ -3,44 +3,50 @@ package se.kth.jazulay.lab4;
 import java.util.Iterator;
 
 import edu.princeton.cs.introcs.In;
+import edu.princeton.cs.introcs.Stopwatch;
 import java.net.URL;
 
 public class Main {
 
-   public static void main(String[] args) {
-      URL url = ClassLoader.getSystemResource("kap1.txt");
+    public static void main(String[] args) {
+        Trie trie = new Trie();
 
-      if (url != null) {
-         System.out.println("Reading from: " + url);
-      } else {
-         System.out.println("Couldn't find file: kap1.txt");
-      }
+        URL url = ClassLoader.getSystemResource("kap1.txt");
 
-      In input = new In(url);
+        if (url != null) {
+            System.out.println("Reading from: " + url);
+        } else {
+            System.out.println("Couldn't find file: kap1.txt");
+        }
 
-      while (!input.isEmpty()) {
-         String line = input.readLine().trim();
-         String[] words = line.split("(\\. )|:|,|;|!|\\?|( - )|--|(\' )| ");
-         String lastOfLine = words[words.length - 1];
+        In input = new In(url);
 
-         if (lastOfLine.endsWith(".")) {
-            words[words.length - 1] = lastOfLine.substring(0, lastOfLine.length() - 1);
-         }
+        Stopwatch sw = new Stopwatch();
+        while (!input.isEmpty()) {
+            String line = input.readLine().trim();
+            String[] words = line.split("(\\. )|:|,|;|!|\\?|( - )|--|(\' )| ");
+            String lastOfLine = words[words.length - 1];
 
-         for (String word : words) {
-            String word2 = word.replaceAll("\"|\\(|\\)", "");
-
-            if (word2.isEmpty()) {
-               continue;
+            if (lastOfLine.endsWith(".")) {
+                words[words.length - 1] = lastOfLine.substring(0, lastOfLine.length() - 1);
             }
+               
+            
+            for (String word : words) {
+                String word2 = word.replaceAll("\"|\\(|\\)", "");
 
-            System.out.println(word2);
-            // Add the word to the trie
-         }
-      }
-   //Perform analysis
-   }
-   
+                if (word2.isEmpty()) {
+                    continue;
+                }
+
+                System.out.println(word2);
+                trie.put(word2);
+            }
+        }
+        System.out.println("\nAdded all words to trie in " + sw.elapsedTime() + " seconds");
+        //Perform analysis
+    }
+
    public static void jonathanTest () {
        Trie t = new Trie();
         t.put("hej");
