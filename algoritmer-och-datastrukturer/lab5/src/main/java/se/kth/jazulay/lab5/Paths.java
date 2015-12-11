1e5088939987ba78dbb219c9d6ec9089635a58fa
Jonathan Azulay <jonathanazulay@me.com>
2015-12-11 17:23:58 +0100

    initial lab5 commit

---
+++ algoritmer-och-datastrukturer/lab5/src/main/java/se/kth/jazulay/lab5/Paths.java
@@ -0,0 +1,11 @@
+package se.kth.jazulay.lab5;
+
+import se.kth.id1020.Graph;
+import se.kth.id1020.DataSource;
+
+public class Paths {
+    public static void main(String[] args) {
+        Graph g = DataSource.load();
+        // work on g
+    }
+}
