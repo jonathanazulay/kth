ff768dbd17a78372f9829b851ae70eb93d083efa
Jonathan Azulay <jonathanazulay@me.com>
2015-12-14 13:56:08 +0100

    begin work with shortestpath algo

---
+++ algoritmer-och-datastrukturer/lab5/src/main/java/se/kth/jazulay/lab5/ShortestPath.java
@@ -0,0 +1,21 @@
+package se.kth.jazulay.lab5;
+
+import se.kth.id1020.Graph;
+
+public class ShortestPath {
+
+    Graph graph;
+
+    public ShortestPath(Graph g) {
+        this.graph = g;
+    }
+
+    public int shortestEW () {
+        return 0;
+    }
+
+    public int shortest () {
+        return 0;
+    }
+
+}
