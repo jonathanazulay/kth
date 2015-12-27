da8f2388ab54c55b70feb8d9999285a3351c644d
Jonathan Azulay <jonathanazulay@me.com>
2015-12-27 16:23:26 +0100

    output formatting

--- algoritmer-och-datastrukturer/lab5/src/main/java/se/kth/jazulay/lab5/Paths.java
+++ algoritmer-och-datastrukturer/lab5/src/main/java/se/kth/jazulay/lab5/Paths.java
@@ -3,13 +3,17 @@ package se.kth.jazulay.lab5;
 import java.util.Arrays;
 import se.kth.id1020.Graph;
 import se.kth.id1020.DataSource;
+import se.kth.id1020.Vertex;
 
 public class Paths {
     public static void main(String[] args) {
         Graph g = DataSource.load();
-        System.out.println(new SubgraphCounter(g).count());
+        System.out.println("How many subgraphs? " + new SubgraphCounter(g).count());
+
+        Vertex[] pathResult = new ShortestPath(g).shortest("Renyn", "Parses");
+        System.out.println("Shortest path contains " + (pathResult.length - 1) + " jumps");
         System.out.println(Arrays.toString(
-            new ShortestPath(g).shortest("Renyn", "Parses")
+            pathResult
         ));
     }
 }
