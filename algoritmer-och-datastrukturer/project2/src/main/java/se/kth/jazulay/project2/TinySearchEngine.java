bf8fb3baa6dbbee414af5a7bbb0f75903a8f7bea
Jonathan Azulay <jonathanazulay@me.com>
2016-01-08 14:18:43 +0100

    add new task

---
+++ algoritmer-och-datastrukturer/project2/src/main/java/se/kth/jazulay/project2/TinySearchEngine.java
@@ -0,0 +1,36 @@
+package se.kth.jazulay.project2;
+
+import java.util.List;
+import se.kth.id1020.TinySearchEngineBase;
+import se.kth.id1020.util.Attributes;
+import se.kth.id1020.util.Document;
+import se.kth.id1020.util.Sentence;
+
+public class TinySearchEngine implements TinySearchEngineBase {
+
+    @Override
+    public void preInserts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void insert(Sentence sntnc, Attributes atrbts) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void postInserts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Document> search(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String infix(String string) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
