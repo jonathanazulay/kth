bd6890a9af5e9a079fe8efcccf7f3398c142a21f
Jonathan Azulay <jonathanazulay@me.com>
2016-01-09 12:45:24 +0100

    build index, WIP

--- algoritmer-och-datastrukturer/project2/src/main/java/se/kth/jazulay/project2/TinySearchEngine.java
+++ algoritmer-och-datastrukturer/project2/src/main/java/se/kth/jazulay/project2/TinySearchEngine.java
@@ -1,12 +1,21 @@
 package se.kth.jazulay.project2;
 
+import java.util.HashMap;
 import java.util.List;
 import se.kth.id1020.TinySearchEngineBase;
 import se.kth.id1020.util.Attributes;
 import se.kth.id1020.util.Document;
 import se.kth.id1020.util.Sentence;
+import se.kth.id1020.util.Word;
 
 public class TinySearchEngine implements TinySearchEngineBase {
+
+    java.util.HashMap<Word, Attributes> words;
+    QueryParser parser = new QueryParser();
+
+    public TinySearchEngine() {
+        this.words = new HashMap();
+    }
 
     @Override
     public void preInserts() {
@@ -15,7 +24,9 @@ public class TinySearchEngine implements TinySearchEngineBase {
 
     @Override
     public void insert(Sentence sntnc, Attributes atrbts) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (Word w : sntnc.getWords()) {
+            this.words.put(w, atrbts);
+        }
     }
 
     @Override
