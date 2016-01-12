ab45fadab0af49c3fbce1a226b4a0e0019a83548
Jonathan Azulay <jonathanazulay@me.com>
2016-01-12 17:21:41 +0100

    first implementation, does not support nested queries

--- algoritmer-och-datastrukturer/project2/src/main/java/se/kth/jazulay/project2/TinySearchEngine.java
+++ algoritmer-och-datastrukturer/project2/src/main/java/se/kth/jazulay/project2/TinySearchEngine.java
@@ -1,48 +1,153 @@
 package se.kth.jazulay.project2;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import se.kth.id1020.TinySearchEngineBase;
 import se.kth.id1020.util.Attributes;
 import se.kth.id1020.util.Document;
 import se.kth.id1020.util.Sentence;
 import se.kth.id1020.util.Word;
+import se.kth.jazulay.project2.RecursiveQueryParser.Query;
 
 public class TinySearchEngine implements TinySearchEngineBase {
 
-    HashMap<Word, Attributes> words;
-    QueryParser parser = new QueryParser();
+    HashMap<String, Set<Document>> wordOccurances;
+    RecursiveQueryParser parser = new RecursiveQueryParser();
 
     public TinySearchEngine() {
-        this.words = new HashMap();
+        this.wordOccurances = new HashMap<>();
     }
 
     @Override
     public void preInserts() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    }
+
+    @Override
+    public void postInserts() {
+
     }
 
     @Override
     public void insert(Sentence sntnc, Attributes atrbts) {
         for (Word w : sntnc.getWords()) {
-            this.words.put(w, atrbts);
+            this.putWord(w, atrbts);
         }
     }
 
-    @Override
-    public void postInserts() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private void putWord (Word w, Attributes atrbts) {
+        Set<Document> occurrences = this.wordOccurances.get(w.word);
+
+        if (occurrences == null) {
+            occurrences = new HashSet<>();
+            this.wordOccurances.put(w.word, occurrences);
+        }
+
+        occurrences.add(atrbts.document);
     }
 
     @Override
     public List<Document> search(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Query query = parser.parse(string);
+        List<Document> sortedResult = null;
+
+        if (query.expression.value != null) {
+            sortedResult = new ArrayList(
+                this.find(query.expression.value)
+            );
+        } else {
+            switch (query.expression.operator) {
+                case MINUS:
+                    sortedResult = new ArrayList(
+                        this.difference(
+                            this.find(query.expression.left.value),
+                            this.find(query.expression.right.value)
+                        )
+                    );
+                    break;
+                case PLUS:
+                    sortedResult = new ArrayList(
+                        this.intersection(
+                            this.find(query.expression.left.value),
+                            this.find(query.expression.right.value)
+                        )
+                    );
+                    break;
+                case OR:
+                    sortedResult = new ArrayList(
+                        this.union(
+                            this.find(query.expression.left.value),
+                            this.find(query.expression.right.value)
+                        )
+                    );
+                    break;
+            }
+        }
+
+        Collections.sort(sortedResult, new Comparator<Document>() {
+            @Override
+            public int compare (Document o1, Document o2) {
+                if (o1.popularity < o2.popularity) { return 1; }
+                else if (o1.popularity > o2.popularity) { return -1; }
+                else { return 0; }
+            }
+        });
+
+        return sortedResult;
     }
 
     @Override
     public String infix(String string) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return parser.parse(string).toString();
     }
 
+    private Set<Document> find (String term) {
+        Set<Document> lookup = this.wordOccurances.get(term);
+        if (lookup == null) { return new HashSet<>(); }
+
+        Set<Document> result = new HashSet<>(lookup.size());
+        for (Document doc : lookup) {
+            result.add(doc);
+        }
+        return result;
+    }
+
+    private Set<Document> intersection (Set<Document> list1, Set<Document> list2) {
+        Set<Document> result = new HashSet<>();
+
+        for (Document doc : list1) {
+            if (list2.contains(doc)) {
+                result.add(doc);
+            }
+        }
+
+        return result;
+    }
+
+    private Set<Document> union (Set<Document> list1, Set<Document> list2) {
+        Set<Document> result = new HashSet<>(list1);
+
+        for (Document doc : list2) {
+            result.add(doc);
+        }
+
+        return result;
+    }
+
+    private Set<Document> difference (Set<Document> list1, Set<Document> list2) {
+        Set<Document> result = new HashSet<>();
+
+        for (Document doc : list1) {
+            if (!list2.contains(doc)) {
+                result.add(doc);
+            }
+        }
+
+        return result;
+    }
 }
